a87e642a9ff03c655df4b2bb491e59901cfb02ce
Shubham Sharma <ss320163@gmail.com>
2020-01-12 20:50:28 +0530

    Added radio buttons

--- Scripts/something.js
+++ Scripts/something.js
@@ -3,6 +3,9 @@ function saveInWebStorage(){
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
     var msg = document.getElementById("msg").value;
+    var gM = document.getElementById("m");
+    var gF = document.getElementById("f");
+    var gO = document.getElementById("o");
 
     if (typeof(Storage) !== "undefined") {
         // Code for localStorage/sessionStorage.
@@ -10,6 +13,9 @@ function saveInWebStorage(){
         localStorage.setItem("Email", email);
         localStorage.setItem("Phone", phone);
         localStorage.setItem("Msg", msg);
+        if(gM.checked == true)  localStorage.setItem("Gender", "MALE");
+        else if(gF.checked ==true) localStorage.setItem("Gender", "FEMALE");
+        else localStorage.setItem("Gender", "OTHER");
         alert("Yippeee-ki-Yay");
     } else {
         // Sorry! No Web Storage support..
--- Scripts/try.js
+++ Scripts/try.js
@@ -2,3 +2,4 @@ document.getElementById("nm").innerHTML = "Name : "+localStorage.getItem("Name")
 document.getElementById("em").innerHTML = "Email : "+localStorage.getItem("Email");
 document.getElementById("ph").innerHTML = "Phone  No. : "+localStorage.getItem("Phone");
 document.getElementById("msg").innerHTML = localStorage.getItem("Msg");
+document.getElementById("gn").innerHTML = "Gender : "+localStorage.getItem("Gender");
